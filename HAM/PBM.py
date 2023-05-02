c2b7a6884712b8bec77e68333e9d7f135e3dce04
Henrik Larsson Hestnes <henrik.hestnes@hotmail.com>
2023-05-02 16:18:02 +0200

    implemented costa functionality

--- HAM/PBM.py
+++ HAM/PBM.py
@@ -1,9 +1,10 @@
 import numpy as np
+import torch
 import asset
 
 
 def make_C_matrix(C_room, C_wall):
-    C1, C2 = np.diag(C_room), np.diag(C_wall)
+    C1, C2 = np.diag(C_room/20), np.diag(C_wall/20)
     return C1, C2
 
 def make_D_matrix(R_internal, R_partWall):
@@ -70,15 +71,15 @@ class thermoPBM():
         rhs_room = self.D @ T_room + self.E @ T_wall + u_room
         rhs_wall = self.F @ T_room + self.G @ T_wall + u_wall
         return rhs_room, rhs_wall
-    
+
     def calculate_lhs(self, T_room, T_wall):
         lhs_room = self.C1 @ T_room
         lhs_wall = self.C2 @ T_wall
         return lhs_room, lhs_wall
     
-    def calculate_T_dot(self, T_room, T_wall, u_room, u_wall):
+    def calculate_T_dot(self, T_room, T_wall, u_room, u_wall, corrective_source_term=np.zeros(13)):
         rhs_room, rhs_wall = self.calculate_rhs(T_room, T_wall, u_room, u_wall)
-        T_dot_room = np.linalg.inv(self.C1) @ rhs_room
+        T_dot_room = np.linalg.inv(self.C1) @ (rhs_room + corrective_source_term)
         T_dot_wall = np.linalg.inv(self.C2) @ rhs_wall
         return T_dot_room, T_dot_wall
     
@@ -88,8 +89,8 @@ class thermoPBM():
         u_wall = np.divide(self.k_2, self.R_room)*(Q_sunPen*Wr+Lr) + np.divide(self.k_1, self.R_ext)*(self.R_ext*T_ext + Q_sunExt + Q_ir)
         return u_room, u_wall
     
-    def step(self, T_room, T_wall, u_room, u_wall, delta_t):
-        T_dot_room, T_dot_wall = self.calculate_T_dot(T_room, T_wall, u_room, u_wall)
+    def step(self, T_room, T_wall, u_room, u_wall, delta_t, corrective_source_term=np.zeros(13)):
+        T_dot_room, T_dot_wall = self.calculate_T_dot(T_room, T_wall, u_room, u_wall, corrective_source_term)
         T_room += delta_t * T_dot_room
         T_wall += delta_t * T_dot_wall
         return T_room, T_wall
